2f04eb51ac2382b35b2a5674395629e525b247f5
pooja <pooja>
2020-05-19 14:05:15 +0530

    Initial Commit

---
+++ MovieApp 2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MovieApp 2
+//
+//  Created by Pooja kumbhar on 19/05/20.
+//  Copyright © 2020 Pooja kumbhar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieApp 2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MovieApp 2
+//
+//  Created by Pooja kumbhar on 19/05/20.
+//  Copyright © 2020 Pooja kumbhar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
